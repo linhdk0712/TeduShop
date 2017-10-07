19ddea100d557a9ef0e14a9e802b32f2af35a7dc
Do Khanh Linh <linhdk0712@gmail.com>
2017-10-07 22:47:53 +0700

    Create Menu,MenuGroup,Product,Footer, Auditable Class, IAuditable Interface

---
+++ TeduShop/TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+        bool Status { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+    }
+}
---
+++ TeduShop/TeduShop.Model/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+    public class Product : Auditable
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        public int CategoryID { get; set; }
+        public string Image { get; set; }
+        public XElement  MoreImage { get; set; }
+        public decimal Price { get; set; }
+        public decimal? PromotionPrice { get; set; }
+        public int? Warranty { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public bool? HomeFlag { get; set; }
+        public bool? HotFlag { get; set; }
+        public int? ViewCount { get; set; }
+
+    }
+}
